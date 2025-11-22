d078deeb83379613825da6bba6085801184fe532
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-11-22 17:05:57 +0000

    Shell-scripts

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,11 +4,11 @@
 #Variable syntax is VAR=DATA but here VAR=NAME and DATA=INPUT
 #Variable names are not case sensetive
 
-PERSON1=vignesh # no space btw and after =
-PERSON2=abhi # to call the variable we need to use ($)
+PERSON1=Vignesh # no space btw and after =
+PERSON2=Abhi # to call the variable we need to use ($)
 
 #Two ways to access Variables, $varname i.e, $PERSON1 or ${PERSON1}.......
-echo $PERSON1 : " Hi $PERSON2, how are you ? "
-echo $PERSON2 : " I am good $PERSON2 and you."
-echo ${PERSON1} : "what's plans tonight."
-echo $PERSON2 : "nothing much. ${PERSON1}"
+echo $PERSON1: " Hi $PERSON2, how are you ? "
+echo $PERSON2: " I am good $PERSON2 and you."
+echo ${PERSON1}: "what's plans tonight."
+echo $PERSON2: "nothing much. ${PERSON1}"
