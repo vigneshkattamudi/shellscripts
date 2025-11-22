e17c26dc0c3730076d989f873b7780aecd0904bf
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-11-22 16:58:16 +0000

    Shell-scripts

--- 02-shebang.sh
+++ 02-shebang.sh
@@ -1,1 +1,7 @@
-echo "Script without Shebang"
+#!/bin/bash
+
+#!/bin/bash--> This is called shebang, It is the first line in the shell script and it is the interpreter to check synatx and excecute commands
+
+echo "#!/bin/bash" 
+
+#-> This is comment
