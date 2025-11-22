de78f0c5c826882cf75c364013d0d8c69c69b9c5
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-11-22 17:28:39 +0000

    Shell-scripts

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,16 +3,6 @@
 # #We can add multiple values in a single variable using arrays.So memory in the computer can be saved.
 # #To comment everything (CTRL+/)
 
-# NAMES=(srinu rani vignesh abhi)
-
-# echo first name : ${NAMES[0]}
-# echo second name : ${NAMES[1]}
-# echo third name : ${NAMES[2]}
-
-# echo all names : ${NAMES[@]}
-
-# #here srinu = 0, rani= 1, vignesh=2, abhi=3
-
 TIMESTAMP=$(date)
 echo "Script started at: $TIMESTAMP"
 
@@ -21,3 +11,5 @@ DEVOPS=("CLOUD" "AWS" "AZURE" "GCP")
 echo "First name is: ${DEVOPS[0]}"
 echo "Second name is: ${DEVOPS[2]}"
 echo "All names is: ${DEVOPS[@]}"
+
+# Here Cloud = 0, Aws= 1, Azure=2, gcp=3
