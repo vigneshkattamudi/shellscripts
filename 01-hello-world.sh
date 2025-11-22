5b0aef1b326f27284d3983aeb92db5d2249a7a28
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-11-22 16:54:53 +0000

    Shell-scripts

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo " This is first shell script" 
-echo "hello Vignesh, Now you are in Linux server"
+echo "This is first shell script" 
+echo "Hello Vignesh, Now you are in Linux server"
 
 #Command to run the script: sh 01.hello_world.sh
-
-echo "Hello Vignesh"
