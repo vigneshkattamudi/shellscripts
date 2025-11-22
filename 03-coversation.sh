b04f1c14d993b53c2299f5a50ae90cea452670e8
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-11-22 17:00:11 +0000

    Shell-scripts

--- 03-coversation.sh
+++ 03-coversation.sh
@@ -4,10 +4,9 @@
 # (#) ->> This is comment
 
 DATE=$(date)
+echo "Script started at : $DATE"
 
-echo "Script started at : ${DATE}"
-
-echo vignesh : " Hi abhi, how are you ? "
-echo abhi : " I am good and you."
-echo vignesh : "what's plans tonight."
-echo abhi : "nothing much."
+echo Vignesh:: "Hi abhi, how are you ? "
+echo Abhi:: "I am good and you."
+echo Vignesh:: "what's plans tonight."
+echo Abhi:: "nothing much."
