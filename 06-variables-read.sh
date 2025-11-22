69070712d1fee69b4a80d61e9b03f0460dd43f1f
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-11-22 17:16:32 +0000

    Shell-scripts

--- 06-variables-read.sh
+++ 06-variables-read.sh
@@ -3,15 +3,21 @@
 #we can use arguments in runtime instead of saving
 #read command is used to pass arguments in run-time
 
-echo "Enter your pin : "
+echo "Enter your pin: "
 read -s PIN # here pin is variable
-echo " pin is : $PIN "
+echo "pin is : $PIN "
 
-echo "Please enter your name"
+echo "Please enter your name:"
 #The Linux read command is used to read the contents of a line into a variable
 read -s USERNAME  # The name entered above will be automatically attached to USERNAME variable. -s will hide the names in terminal
 echo "The user name is: $USERNAME" #Prinitng on screen for reference
 
-echo "Please enter your password"
+echo "Please enter your password:"
 read -s PASSWORD  
 echo "The password is: $PASSWORD" #Prinitng on screen for reference
+
+echo "please enter you job role:"
+
+read jobrole
+
+echo "My jobrole is $jobrole"
