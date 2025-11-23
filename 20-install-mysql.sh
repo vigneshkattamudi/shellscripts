80c668861c87b908885d330ece77ed10589420ba
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-11-23 21:19:48 +0000

    shell

--- 20-install-mysql.sh
+++ 20-install-mysql.sh
@@ -20,45 +20,45 @@ VALIDATE(){
     fi
 }
 
-echo "Script executed at: $(date)"  &>> $LOG_FILE
+echo "Script executed at: $(date)"  &>>$LOG_FILE
 
 USERID=$(id -u)
 if [ $USERID -eq 0 ]
 then
-    echo -e "User is Root $G Proceed $N, So install MySQL" &>> $LOG_FILE
+    echo -e "User is Root $G Proceed $N, So install MySQL" &>>$LOG_FILE
 else
-    echo -e "ERR: $R User is not root user $N, Run with Root" &>> $LOG_FILE
+    echo -e "ERR: $R User is not root user $N, Run with Root" &>>$LOG_FILE
     exit 1 # other than zero is failure
 fi
 
-dnf list installed mysql &>> $LOG_FILE
+dnf list installed mysql &>>$LOG_FILE
 if [ $? -eq 0 ]
 then
-    echo -e " $Y Already MySQL installed...Nothing to do $N" &>> $LOG_FILE
+    echo -e " $Y Already MySQL installed...Nothing to do $N" &>>$LOG_FILE
 else
-    echo "Need to install MySQL" &>> $LOG_FILE
+    echo "Need to install MySQL" &>>$LOG_FILE
     dnf install mysql -y
-    VALIDATE $? "mysql" &>> $LOG_FILE
+    VALIDATE $? "mysql" &>>$LOG_FILE
 fi
 
 dnf list installed nginx
 if [ $? -eq 0 ]
 then
-    echo -e " $Y Already nginx installed...Nothing to do $N" &>> $LOG_FILE
+    echo -e " $Y Already nginx installed...Nothing to do $N" &>>$LOG_FILE
 else
-    echo "Need to install nginx" &>> $LOG_FILE
+    echo "Need to install nginx" &>>$LOG_FILE
     dnf install nginx -y
-    VALIDATE $? "nginx" &>> $LOG_FILE
+    VALIDATE $? "nginx" &>>$LOG_FILE
 fi
 
 dnf list installed python3
 if [ $? -eq 0 ]
 then
-    echo -e  "$Y Already python3 installed...Nothing to do $N" &>> $LOG_FILE
+    echo -e  "$Y Already python3 installed...Nothing to do $N" &>>$LOG_FILE
 else
-    echo "Need to install python3" &>> $LOG_FILE
+    echo "Need to install python3" &>>$LOG_FILE
     dnf install python3 -y
-    VALIDATE $? "python3" &>> $LOG_FILE
+    VALIDATE $? "python3" &>>$LOG_FILE
 fi
 #Reverse of if indicates ending of condition.
 #Shell script won't stop if it faces errors, all other languages will stop. 
