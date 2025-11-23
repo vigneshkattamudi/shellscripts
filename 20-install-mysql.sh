b3a24af61bc9b6ccc5421085ed77c1e0ddf364c1
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-11-23 21:20:51 +0000

    shell

--- 20-install-mysql.sh
+++ 20-install-mysql.sh
@@ -7,7 +7,7 @@ N="\e[0m"
 
 mkdir -p LOGS_FOLDER
 
-LOGS_FOLDER="/var/log/shell-scripts"
+LOGS_FOLDER="/var/logs/shell-scripts"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 VALIDATE(){
