e2499cb6edb931ec3b419845efe4d664b3a3ae7f
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-11-24 19:47:20 +0000

    shell

--- 20-install-mysql.sh
+++ 20-install-mysql.sh
@@ -6,7 +6,7 @@ Y="\e[33m"
 N="\e[0m"
 
 LOGS="/var/log/shell-scripts"
-SCRIPT_NAME=$( $0 | cut -d "." -f1)
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS
