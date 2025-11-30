89caf6cf120175df1524e3d10ee0ccaeac0c730e
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-11-30 16:04:16 +0000

    shell

--- script-2.sh
+++ script-2.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+START_TIME=$(date +%s)
 
 echo "PID of script-2: $$"
 
 echo "$GREETING"
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=(($END_TIME - $START_TIME))
+echo "Total time taken: $TOTAL_TIME seconds"
