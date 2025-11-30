91c6908690fd4e2d1f315754019ee8032448bca8
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-11-30 15:59:21 +0000

    shell

--- 20-install-mysql.sh
+++ 20-install-mysql.sh
@@ -1,47 +1,34 @@
 #!/bin/bash
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+N='\e[34m'
 
-LOGS_FOLDER="/var/logs/shellscript-logs"     # Folder to store logs
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )    # Extract script name without extension
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"     # Define full path for log file
-PACKAGES=("mysql" "nginx" "python3")
-
-mkdir -p $LOGS_FOLDER                         # Create logs directory if it doesn't exist
-echo "Script started Executing at : $(date)" | tee -a $LOG_FILE   # Log script start time
-
-USERID=$(id -u)                               # Get current user ID
-if [ $USERID -eq 0 ]                          # Check if user is root
+USERID=$(id -u)
+if [ $USERID -eq 0 ]
 then
-    echo -e "User is root...$G Proceed $N" | tee -a $LOG_FILE      # If root, log and continue
+    echo -e "$G User root... Proceed $N"
 else
-    echo -e "$R ERROR : Run with Root $N " | tee -a $LOG_FILE      # If not root, log error
-    exit 1                                                          # Exit script
+    echo -e "$R ERR: Run with root $N"
+
 fi
 
-VALIDATE(){                                                    # Function to validate commands
-if [ $1 -eq 0 ]                                                 # Check exit status
-then
-    echo -e "Installing $2...$G SUCCESS $N" | tee -a $LOG_FILE  # Log success message
-else
-    echo -e "INSTALLATION $2..$R FAILED $N" | tee -a $LOG_FILE  # Log failure message
-    exit 1                                                      # Exit script
-fi
+LOGS="/var/log/rosboshop-logs/"
+LOG_FOLDER=$( echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS/$LOG_FOLDER.log"
+SCRIPT=$PWD
+
+mkdir -p $LOGS
+echo "Script started at $(date)" | tee -a $LOG_FILE
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
 }
 
-#for package in ${PACKAGES[@]}
-for package in $@
-do 
-    dnf list installed $package &>>$LOG_FILE       # Check if MySQL is installed and log output
-    if [ $? -eq 0 ]                             # If installed
-    then
-        echo -e "Nothing to do...$Y $package installed $N" | tee -a $LOG_FILE  # Log already installed
-    else
-        echo "$package NOT installed.. Installing Now" | tee -a $LOG_FILE            # Log installation start
-        dnf install mysql -y &>>$LOG_FILE                                   # Install MySQL and log
-        VALIDATE $? $package                                                   # Validate installation
-    fi
-done
