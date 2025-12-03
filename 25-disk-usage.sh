e2ff4dcbfd3c9f00c022fb1533684e923ddcc657
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-12-03 11:51:06 +0000

    shell

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -7,7 +7,7 @@ while IFS= read -r line
 do
     USAGE=$(echo $line | awk -F '{ print $6 }' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F '{ print $6 }')
-    MSG+=$(echo "High disk usage in $PARTITION : $USAGE % \n)
+    MSG+=$(echo "High disk usage in": $PARTITION : $USAGE % \n)
 done <<< $DISK_USAGE
 
-echo "$MSG"
+echo $MSG
