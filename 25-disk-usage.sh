9a4b3c3a3b73d714eefe9cde367a913c9d502b8e
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-12-03 11:53:26 +0000

    shell

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -5,8 +5,12 @@ THRESHOLD=1
 
 while IFS= read -r line
 do
-    USAGE=$(echo $line | awk -F '{ print $6 }' | cut -d "%" -f1)
-    PARTITION=$(echo $line | awk -F '{ print $6 }')
+    USAGE=$(echo $line | awk -F '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk -F '{print $7F}')
+     if [ $USAGE -ge $THRESHOLD ]
+    then
+        MSG+="High Disk Usage on $PARTITION: $USAGE % <br>" #<br> represents HTML new
+    fi
     MSG+=$(echo "High disk usage in": $PARTITION : $USAGE % \n)
 done <<< $DISK_USAGE
 
