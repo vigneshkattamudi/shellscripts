6c7acbc7a26901edd413dcd43d4a45963e1a0b25
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-12-05 17:02:30 +0000

    shell

--- 23-disk-usage.sh
+++ 23-disk-usage.sh
@@ -3,7 +3,7 @@
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 # in project it will be 75
 MSG=""
-IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4) # ( hostname -I )
 
 while IFS= read line
 do
